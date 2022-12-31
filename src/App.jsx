4c799037c2e9e60c6a49544c132c5cf6dc97a22f
Adonis De Paula <a.depaula@gbh.com.do>
2022-12-31 10:03:42 -0400

    [Feat] Add cardInput component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import InputCard from './components/InputCard/InputCard'
+import Table from "./components/Table/Table"
 import "./style.css";
 function App() {
+  
+/*C = cantidad cuota
+V = monto prestamo
+N = numero cuotas
+i = interes mensual (interesanual/12)/100
+  */
+    const [state, setState] = useState({C:0, V:"", N: "", i:''});
 
   return (
     <div className="App">
-
-      <InputCard />
-
+      <InputCard state={state} setState={setState} />
+      <Table />
     </div>
   )
 }
--- src/components/InputCard/InputCard.jsx
+++ src/components/InputCard/InputCard.jsx
@@ -3,25 +3,25 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { InputAdornment, TextField } from "@mui/material";
-import { useState } from "react";
 
-const InputCard = () => {
+
+const InputCard = ({state, setState}) => {
   const style = {
     display: "flex",
     flexDirection: "column",
   };
-  const[amount, setAmount] = useState("");
-  const[cuotes, setCuotes ] = useState("");
-  const[interst, setInterest]= useState("");
 
 
-
-  const handleChange = (e,setNum) => {
+  const handleChange = (e, setter, currentState) => {
     const regex = /^[0-9\b]+$/;
     if (e.target.value == "" || regex.test(e.target.value)) {
-      setNum(e.target.value);
+      setter(prevState => ({
+        ...prevState,
+        [currentState]: e.target.value
+      }));
     }
   };
+  
 
   return (
     <Card>
@@ -36,26 +36,24 @@ const InputCard = () => {
             InputProps={{
               endAdornment: <InputAdornment position="start">$</InputAdornment>,
             }}
-            onChange={(e) => handleChange(e, setAmount)}
-            value={amount}
-            
+            onChange={(e) => handleChange(e, setState, "V")}
+            value={state.V}
           ></TextField>
           <TextField
             variant="outlined"
             label="Cuotas"
             size="small"
             style={{ marginTop: "3px", marginBottom: "1em" }}
-            onChange={(e) => handleChange(e, setCuotes)}
-            value={cuotes}
-            
+            onChange={(e) => handleChange(e, setState, "N")}
+            value={state.N}
           ></TextField>
           <TextField
             variant="outlined"
             label="Interes"
             size="small"
             style={{ marginTop: "3px", marginBottom: "1em" }}
-            onChange={(e) => handleChange(e, setInterest)}
-            value={interst}
+            onChange={(e) => handleChange(e, setState, "i")}
+            value={state.i}
           ></TextField>
         </div>
         <Button variant={"contained"} color="primary">
